44cd028da63e588c1c573457cad710a153662ca0
TheNeekOfficial <dylan_leslie05@yahoo.com.au>
2025-01-09 23:03:36 +1100

    Change when discord starts

--- .config/nvim/lua/plugins/discord.lua
+++ .config/nvim/lua/plugins/discord.lua
@@ -3,6 +3,7 @@ return {
 		"vyfor/cord.nvim",
 		branch = "client-server",
 		build = ":Cord update",
+		event = { "VimStart" "BufEnter" },
 		config = function()
 			local blacklist = {
 				"/home/dylan/",

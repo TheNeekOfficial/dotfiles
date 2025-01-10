a02dc1e627e716cdf2345bd5e3893f7c555e7771
TheNeekOfficial <dylan_leslie05@yahoo.com.au>
2025-01-10 22:02:11 +1100

    just actually disabled mouse

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -16,7 +16,7 @@ vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 -- vim.opt.mouse = 'a'
-vim.set.mouse = ""
+vim.opt.mouse = ''
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false

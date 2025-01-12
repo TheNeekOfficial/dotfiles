80958cee6bea9555639bfd6401b664874ec49a9e
TheNeekOfficial <dylan_leslie05@yahoo.com.au>
2025-01-12 13:55:16 +1100

    Fixed erros w/ recent lsp update

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -227,9 +227,9 @@ return {
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
 				"stylua", -- Used to format Lua code
-        "python-lsp-server" -- For python
-        "nil_ls" -- Nix
-        "rnix" -- Nix
+        "python-lsp-server", -- For python
+        "nil_ls", -- Nix
+        "rnix", -- Nix
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 

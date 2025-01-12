4f71789a14c9f255f23d1be830c6fd8796d4a0da
TheNeekOfficial <dylan_leslie05@yahoo.com.au>
2025-01-12 20:32:50 +1100

    Removed lua settings having looked at docs

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -261,18 +261,7 @@ return {
 			  local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 			  lspconfig.nixd.setup({ capabilities = lsp_capabilities })
 			  lspconfig.pylsp.setup({ capabilities = lsp_capabilities })
-			  lspconfig.lua_ls.setup({
-			    capabilities = lsp_capabilities
-					settings = {
-						Lua = {
-							completion = {
-								callSnippet = "Replace",
-							},
-							-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-							-- diagnostics = { disable = { 'missing-fields' } },
-						},
-					},
-			  })
+			  lspconfig.lua_ls.setup({ capabilities = lsp_capabilities })
       end
 
 		end,

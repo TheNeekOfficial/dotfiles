8333c83d52dc8a0ee98dad41844e230005e32289
TheNeekOfficial <dylan_leslie05@yahoo.com.au>
2025-01-12 16:16:10 +1100

    Trying to get non-mason lsp's working

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -43,7 +43,7 @@ return {
 			
 			-- Nix setup
 			local lspconfig = require("lspconfig")
-			local lsp_capabilities = require("cmp-nvim-lsp").default_capabilities()
+			local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 			lspconfig.nixd.setup({ capabilities = lsp_capabilities })
 
 			cmp.setup({

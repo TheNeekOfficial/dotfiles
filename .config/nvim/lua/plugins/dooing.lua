b1e26f2998f783773ff6954b36766922d41400ac
TheNeekOfficial <dylan_leslie05@yahoo.com.au>
2025-01-14 21:21:31 +1100

    Added dooing.lua

---
+++ .config/nvim/lua/plugins/dooing.lua
@@ -0,0 +1,18 @@
+return {
+-- minimal installation
+{
+  "Hashino/doing.nvim",
+  cmd = "Do",
+  keys = {
+    { "<leader>da", function() require("doing").add() end, {}, },
+    { "<leader>dn", function() require("doing").done() end, {}, },
+    { "<leader>de", function() require("doing").edit() end, {}, },
+  },
+}
+-- Lualine setup
+require("lualine").setup {
+  winbar = {
+    lualine_a = { require("doing").status },
+  },
+}
+}

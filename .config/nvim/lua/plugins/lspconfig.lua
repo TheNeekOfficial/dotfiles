96692508b734b0b5e1f37eca8b0534294805e78f
TheNeekOfficial <dylan_leslie05@yahoo.com.au>
2025-03-25 08:53:57 +1100

    lspconfig update

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -261,7 +261,8 @@ return {
 				local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 				lspconfig.nixd.setup({ capabilities = lsp_capabilities })
 				lspconfig.pylsp.setup({ capabilities = lsp_capabilities })
-        lspconfig.rust_analyzer.setup({ capabilities = lsp_cabilities })
+        lspconfig.rust_analyzer.setup({ capabilities = lsp_capabilities })
+        lspconfig.lua_ls.setup({ capabilities = lsp_capabilities })
 
 				-- NOTE: Can add mason setup stuff just for lua_ls if really matters
 				-- Or just desperately try to get it working
